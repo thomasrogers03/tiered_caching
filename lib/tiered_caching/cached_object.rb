e6de688223029a7cbed013dcd193c0a89c7c4e79
Thomas Rogers <ThomasRogers03@gmail.com>
2015-06-25 21:55:00 -0400

    Implement CachedObject.[]= to cache instances of the base class including this module

--- lib/tiered_caching/cached_object.rb
+++ lib/tiered_caching/cached_object.rb
@@ -5,6 +5,12 @@ module TieredCaching
       CacheMaster.get(class: self, key: key)
     end
 
+    def []=(key, value)
+      raise TypeError, "Cannot convert #{value.class} into #{self}" unless value.is_a?(self)
+
+      CacheMaster.set(class: self, key: key) { value }
+    end
+
   end
 
   module CachedObject
--- spec/classes/cached_object_spec.rb
+++ spec/classes/cached_object_spec.rb
@@ -11,14 +11,14 @@ module TieredCaching
       include CachedObject
     end
 
+    let(:key) { 'key' }
+    let(:object) { subject.new }
+
     subject { MockObject }
 
     before { CacheMaster << global_store }
 
     describe '.[]' do
-      let(:key) { 'key' }
-      let(:object) { subject.new }
-
       it 'should retrieve the object from the CacheMaster' do
         CacheMaster.set(class: MockObject, key: key) { object }
         expect(subject[key]).to eq(object)
@@ -39,6 +39,45 @@ module TieredCaching
         it 'should retrieve the object from the CacheMaster' do
           CacheMaster.set(class: OtherMockObject, key: key) { object }
           expect(subject[key]).to eq(object)
+        end
+      end
+    end
+
+    describe '.[]=' do
+      it 'should save the object using the class and specified key as the CacheMaster key' do
+        subject[key] = object
+        expect(CacheMaster.get(class: MockObject, key: key)).to eq(object)
+      end
+
+      context 'with a different key' do
+        let(:key) { 'other key' }
+
+        it 'should cache the object' do
+          subject[key] = object
+          expect(CacheMaster.get(class: MockObject, key: key)).to eq(object)
+        end
+      end
+
+      context 'with a different class' do
+        subject { OtherMockObject }
+
+        it 'should cache the object' do
+          subject[key] = object
+          expect(CacheMaster.get(class: OtherMockObject, key: key)).to eq(object)
+        end
+      end
+
+      context 'with the wrong type of object specified' do
+        it 'should raise an error' do
+          expect{subject[key] = 'bad type'}.to raise_error(TypeError, 'Cannot convert String into TieredCaching::MockObject')
+        end
+
+        context 'with a different type combination' do
+          subject { OtherMockObject }
+
+          it 'should raise an error' do
+            expect{subject[key] = 1337}.to raise_error(TypeError, 'Cannot convert Fixnum into TieredCaching::OtherMockObject')
+          end
         end
       end
     end
