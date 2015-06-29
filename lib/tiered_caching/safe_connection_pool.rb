1545cb55e20acea12f240992d82568fd82628311
Thomas Rogers <ThomasRogers03@gmail.com>
2015-06-28 22:13:49 -0400

    SafeConnectionPool now disables itself when an error is raised within a called block

--- lib/tiered_caching/safe_connection_pool.rb
+++ lib/tiered_caching/safe_connection_pool.rb
@@ -1,6 +1,5 @@
 module TieredCaching
   class SafeConnectionPool
-
     def initialize(internal_pool)
       @internal_pool = internal_pool
     end
@@ -9,10 +8,21 @@ module TieredCaching
       @internal_pool.with { |conn| safe_with(conn, &block) }
     end
 
+    def disabled?
+      !!@disabled
+    end
+
     private
 
     def safe_with(conn)
-      yield conn rescue nil
+      return nil if disabled?
+
+      begin
+        yield conn
+      rescue
+        @disabled = true
+        nil
+      end
     end
 
   end
--- spec/classes/safe_connection_pool_spec.rb
+++ spec/classes/safe_connection_pool_spec.rb
@@ -14,6 +14,11 @@ module TieredCaching
         expect(result_connection).to eq(connection)
       end
 
+      it 'should leave the pool in a good state' do
+        subject.with {  }
+        expect(subject.disabled?).to eq(false)
+      end
+
       context 'with a different block' do
         it 'should return the result of that block' do
           result = subject.with { 135 }
@@ -24,6 +29,17 @@ module TieredCaching
       context 'with an error' do
         it 'should return nil' do
           result = subject.with { raise 'It blew up!' }
+          expect(result).to be_nil
+        end
+
+        it 'should put the pool in a bad state' do
+          subject.with { raise 'It blew up!' }
+          expect(subject.disabled?).to eq(true)
+        end
+
+        it 'should return nil for all further requests' do
+          subject.with { raise 'It blew up!' }
+          result = subject.with { 157 }
           expect(result).to be_nil
         end
       end
